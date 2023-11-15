ec2f5cf3d0a92451736a37ac06c721b0fdc35340
HunggSamm <hunggsamm@gmail.com>
2023-11-15 20:08:06 +0700

    begin

--- AI.py
+++ AI.py
@@ -4,7 +4,7 @@ from pygame.locals import *
 
 pygame.init()
 
-WIDTH, HEIGHT = 500, 500
+WIDTH, HEIGHT = 600, 600
 GRID_SIZE = 20
 PLAYER_SIZE = 20
 
@@ -12,7 +12,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 font = pygame.font.Font(None, 36)
-button1_rect = pygame.Rect(330, 10, 120, 50)
+button1_rect = pygame.Rect(470, 10, 120, 50)
 
 visited_cells = []  # New list to store visited cells during DFS
 
@@ -26,21 +26,21 @@ def drawButtons():
     screen.blit(button1_text_surface, button1_text_rect)
 
 maze = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
-    [1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
-    [1, 1, 3, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
-    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+    [1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [1, 1, 3, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
 entrance_pos = [(row, col) for row in range(len(maze)) for col in range(len(maze[0])) if maze[row][col] == 2][0]
@@ -58,7 +58,11 @@ def dfs_player2(maze, current_pos, visited, steps):
     row, col = current_pos
     visited[row][col] = True
     visited_cells.append((row, col))  # Store the visited cell
-
+    maze[row][col] = 4  # Mark the path in the maze
+    pygame.time.delay(100)  # Delay for visualization
+    draw_maze(maze, (255, 255, 0))  # Draw the maze with the path
+    pygame.display.flip()
+    pygame.event.pump()
     if current_pos == exit_pos:
         steps.append((row, col))
         return True
@@ -84,7 +88,24 @@ dfs_steps_player2 = []
 current_step_player2 = 0
 
 auto_mode = False
+def draw_maze(maze, player_color):
+    screen.fill(WHITE)
+    for row in range(len(maze)):
+        for col in range(len(maze[0])):
+            if maze[row][col] == 1:
+                pygame.draw.rect(screen, BLACK, (col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+            elif maze[row][col] == 2:
+                pygame.draw.rect(screen, (0, 255, 0), (col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+            elif maze[row][col] == 3:
+                pygame.draw.rect(screen, (255, 0, 0), (col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+            elif maze[row][col] == 4:  # Path color
+                pygame.draw.rect(screen, player_color, (col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE))
 
+    # Draw the players
+    pygame.draw.rect(screen, (0, 0, 255), (player1_x * GRID_SIZE, player1_y * GRID_SIZE, PLAYER_SIZE, PLAYER_SIZE))
+    pygame.draw.rect(screen, (255, 0, 0), (player2_x * GRID_SIZE, player2_y * GRID_SIZE, PLAYER_SIZE, PLAYER_SIZE))
+
+    drawButtons()
 while True:
     drawButtons()
     for event in pygame.event.get():
@@ -96,6 +117,7 @@ while True:
             if button1_rect.collidepoint(mouse_x, mouse_y):
                 auto_mode = True
                 solvable = dfs_player2(maze, (player2_y, player2_x), visited_player2, dfs_steps_player2)
+
         elif not auto_mode and event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP and player1_y > 0 and maze[player1_y - 1][player1_x] != 1:
                 player1_y -= 1
@@ -115,19 +137,10 @@ while True:
             elif event.key == pygame.K_d and player2_x < len(maze[0]) - 1 and maze[player2_y][player2_x + 1] != 1:
                 player2_x += 1
 
-    screen.fill(WHITE)
-    for row in range(len(maze)):
-        for col in range(len(maze[0])):
-            if maze[row][col] == 1:
-                pygame.draw.rect(screen, BLACK, (col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-            elif maze[row][col] == 2:
-                pygame.draw.rect(screen, (0, 255, 0), (col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-            elif maze[row][col] == 3:
-                pygame.draw.rect(screen, (0, 0, 255), (col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-
+    draw_maze(maze,(0, 0, 255))
     for row, col in visited_cells:
         pygame.draw.rect(screen, (255, 255, 0), (col * GRID_SIZE, row * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-    pygame.draw.rect(screen, (0, 0, 255), (player1_x * GRID_SIZE, player1_y * GRID_SIZE, PLAYER_SIZE, PLAYER_SIZE))
+
 
     if auto_mode and current_step_player2 < len(dfs_steps_player2):
         current_pos_player2 = dfs_steps_player2[current_step_player2]
@@ -135,7 +148,6 @@ while True:
         player2_y, player2_x = current_pos_player2
         current_step_player2 += 1
 
-    pygame.draw.rect(screen, (255, 0, 0), (player2_x * GRID_SIZE, player2_y * GRID_SIZE, PLAYER_SIZE, PLAYER_SIZE))
 
     drawButtons()
     pygame.display.flip()
